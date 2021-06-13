c9d4ad171a997a3f1e7d1b847c7318a328fc176c
Олег Смаранди <olegsmarandi@gmail.com>
2021-06-13 23:34:03 +0300

    reset function fixed & send photo method excluded

--- bot.py
+++ bot.py
@@ -60,18 +60,17 @@ def generate_command(message):
     gen = preview_gen.PreviewGenerator()
     gen.run(f"{message.chat.id}")
     bot.send_chat_action(message.chat.id, 'typing')
-    try:
-        bot.send_photo(message.chat.id, open(os.path.join(f"{message.chat.id}", "preview.png"), 'rb'))
+    try:    
         bot.send_document(message.chat.id, open(os.path.join(f"{message.chat.id}", "preview.png"), 'rb'))
         clear_all(f"{message.chat.id}")
     except Exception as e:
         print(e.args)
         bot.send_message(message.chat.id, "Failed to generate preview...")
-            
+
 
 @bot.message_handler(commands=["reset"])
 def reset(message):
-    clear_all(message.chat.id)
+    clear_all(f"{message.chat.id}")
 
 
 if __name__ == "__main__":
